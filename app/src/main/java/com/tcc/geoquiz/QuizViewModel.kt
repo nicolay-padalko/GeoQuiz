09f021c9e67f9073770d9db2651f0f0195787d22
nicolay-padalko <nicolay.padalko@gmail.com>
2020-09-28 13:07:23 -0300

    Initial commit

---
+++ app/src/main/java/com/tcc/geoquiz/QuizViewModel.kt
@@ -0,0 +1,32 @@
+package com.tcc.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+
+    var currentIndex = 0
+    var isCheater = false
+
+    private val questionBank = listOf(
+        Question(R.string.question_brasil, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+        Question(R.string.question_africa, false))
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+    fun moveToPrevious() {
+        currentIndex = (currentIndex - 1) % questionBank.size
+    }
+}
